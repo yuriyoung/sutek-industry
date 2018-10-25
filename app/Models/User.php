61e55bd3ab101c9ab1cc090a88ace8b62cb2cab7
Yuri Young <yuri.young@qq.com>
2018-10-25 18:57:14 +0800

    customize use id and permission guard

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,10 +4,15 @@ namespace App\Models;
 
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Spatie\Permission\Traits\HasRoles;
+use Keygen\Keygen;
 
 class User extends Authenticatable
 {
     use Notifiable;
+    use HasRoles;
+
+    protected $guard_name = 'web';
 
     /**
      * The attributes that are mass assignable.
@@ -18,6 +23,8 @@ class User extends Authenticatable
         'name', 'email', 'password',
     ];
 
+    public $incrementing = false;
+    
     /**
      * The attributes that should be hidden for arrays.
      *
@@ -26,4 +33,32 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    protected static function boot ()
+    {
+        parent::boot();
+
+        static::creating(function (User $user) {
+            $id = $user->_generatePrimaryKey();
+            while (Product::whereId($id)->count() > 0)
+            {
+                $id = $user->_generatePrimaryKey();
+            }
+            $user->id = $id;
+        });
+    }
+
+    /**
+     * @brief 用于随机生成产品的9位主键id
+     * 依赖库: composer require gladcodes/keygen，见: https://github.com/gladchinda/keygen-php
+     * @Author: Yuri Young<yuri.young@qq.com>
+     * @return string
+     */
+    private function _generatePrimaryKey()
+    {
+        return Keygen::numeric(7)
+            ->prefix(mt_rand(1, 9))
+            ->suffix(mt_rand(1, 9))
+            ->generate(true);
+    }
 }
